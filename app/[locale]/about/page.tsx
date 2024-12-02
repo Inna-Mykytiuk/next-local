4f6da9cae5151a777190d8c8a35ef69acc0bbdbd
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-12-02 17:17:36 +0200

    add about page with pathname

---
+++ app/[locale]/about/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>
+        About Page
+      </h1>
+
+      <Link href="/">Home</Link>
+    </div>
+  )
+}
+
